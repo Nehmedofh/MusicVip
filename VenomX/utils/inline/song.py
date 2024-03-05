05973dd6e041a24137ede51cfbd387b55b30ba52
Nehmedofh <157984271+Nehmedofh@users.noreply.github.com>
2024-03-06 01:18:47 +0400

    Update song.py

--- VenomX/utils/inline/song.py
+++ VenomX/utils/inline/song.py
@@ -15,7 +15,7 @@ def song_markup(_, vidid):
         ],
         [
             InlineKeyboardButton(
-                text="🥀 sᴜᴩᴩᴏʀᴛ 🥀", url=f"{config.SUPPORT_CHAT}",
+                text="Rəsmi Kanal 🌹", url=f"{config.SUPPORT_CHAT}",
             ),
             InlineKeyboardButton(
                 text=_["CLOSE_BUTTON"], callback_data="close"
