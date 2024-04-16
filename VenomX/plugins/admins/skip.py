a44e72de5f66508904e610ad681ce17640d3ffb2
yusiqo <80514373+yusiqo@users.noreply.github.com>
2024-04-16 14:44:19 +0400

    Update skip.py

--- VenomX/plugins/admins/skip.py
+++ VenomX/plugins/admins/skip.py
@@ -16,7 +16,6 @@ from config import BANNED_USERS
 @app.on_message(
     filters.command(["skip", "cskip", "next", "cnext"]) & filters.group & ~BANNED_USERS
 )
-@AdminRightsCheck
 async def skip(cli, message: Message, _, chat_id):
     if not len(message.command) < 2:
         loop = await get_loop(chat_id)
