71217853dbc285d43f16e2f1598addd2822a39d3
Nehmedofh <157984271+Nehmedofh@users.noreply.github.com>
2024-04-16 14:53:56 +0400

    Update play.py

--- VenomX/utils/inline/play.py
+++ VenomX/utils/inline/play.py
@@ -23,7 +23,7 @@ def track_markup(_, videoid, user_id, channel, fplay):
                 text="👨🏻‍💻 Əlaqə", url=f"tg://openmessage?user_id={OWNER_ID}",
             ),
             InlineKeyboardButton(
-                text="Rəsmi Kanal 🧸", url=SUPPORT_CHANNEL,
+                text="Rəsmi Kanal 🧸", url=SUPPORT_CHAT,
             )
         ],
         [
@@ -80,7 +80,7 @@ def stream_markup_timer(_, chat_id, played, dur):
                 text="👨🏻‍💻 Əlaqə", url=f"tg://openmessage?user_id={OWNER_ID}",
             ),
             InlineKeyboardButton(
-                text="Rəsmi Kanal 🧸", url=SUPPORT_CHANNEL,
+                text="Rəsmi Kanal 🧸", url=SUPPORT_CHAT,
             )
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
@@ -102,7 +102,7 @@ def stream_markup(_, chat_id):
                 text="👨🏻‍💻 Əlaqə", url=f"tg://openmessage?user_id={OWNER_ID}",
             ),
             InlineKeyboardButton(
-                text="Rəsmi Kanal 🧸", url=SUPPORT_CHANNEL,
+                text="Rəsmi Kanal 🧸", url=SUPPORT_CHAT,
             )
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
