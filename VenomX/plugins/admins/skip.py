fba818cbfcf3299978c625b19005a2781c178f56
yusiqo <80514373+yusiqo@users.noreply.github.com>
2024-04-16 14:40:47 +0400

    Update skip.py

--- VenomX/plugins/admins/skip.py
+++ VenomX/plugins/admins/skip.py
@@ -20,8 +20,6 @@ from config import BANNED_USERS
 async def skip(cli, message: Message, _, chat_id):
     if not len(message.command) < 2:
         loop = await get_loop(chat_id)
-        if loop != 0:
-            return await message.reply_text(_["admin_8"])
         state = message.text.split(None, 1)[1].strip()
         if state.isnumeric():
             state = int(state)
